cbcb989b300f935b6aa85837cfd0886b4908d051
Luis Rollón Gordo <luisrollongordo@gmail.com>
2018-03-05 13:49:35 +0100

    Final commit

--- FacebookLoginDemo_Inicio/ViewController.swift
+++ FacebookLoginDemo_Inicio/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginWithFacebook(_ sender: Any) {
+        let fbLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+            if let error = error {
+                print("Failed to login: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let accessToken = FBSDKAccessToken.current() else {
+                print("Failed to get access token")
+                return
+            }
+            
+            let credential = FIRFacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+            
+            // Perform login by calling Firebase APIs
+            FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+                if let error = error {
+                    print("Login error: \(error.localizedDescription)")
+                    let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                    let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(okayAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                    return
+                }
+                
+                // Present the Welcome View Controller
+                if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeViewController") {
+                    UIApplication.shared.keyWindow?.rootViewController = viewController
+                    self.dismiss(animated: true, completion: nil)
+                }
+                
+            })
+            
+        }
+
+    }
 
 }
+
+
+
 
---
+++ Pods/Target Support Files/Pods-FacebookLoginDemo_Inicio/Pods-FacebookLoginDemo_Inicio-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FacebookLoginDemo_InicioVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FacebookLoginDemo_InicioVersionString[];
+
